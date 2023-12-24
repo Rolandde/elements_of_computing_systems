18acf3aa0bbf7f1028c62f3fd0075fddfa5c9163
Savo Lazic <lazicsavo@gmail.com>
2023-12-24 15:58:12 -0500

    Rewrote virtual machine parser
    The big switch was from an internal vec storage to a struct field
    storage. This made parsing logic less verbose and allowed for cleaner
    error handling.

--- hack_virtual_machine/src/lib.rs
+++ hack_virtual_machine/src/lib.rs
@@ -1,3 +1,33 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Command {
+    Add,
+    Subtract,
+    Negative,
+    Equal,
+    GreaterThan,
+    LessThan,
+    BitAnd,
+    BitOr,
+    BitNot,
+    Goto(String),
+    If(String),
+    Label(String),
+    Pop(Segment, u16),
+    Push(Segment, u16),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Segment {
+    Argument,
+    Local,
+    Static,
+    Constant,
+    This,
+    That,
+    Pointer,
+    Temp,
+}
+
 /// Errors during VM functioning
 #[derive(Debug)]
 pub enum Error {
@@ -7,16 +37,19 @@ pub enum Error {
     Io(std::io::Error),
     /// The command is not part of the VM spec
     UnknownCommand(u16),
+    /// The segement is not part of the VM spec
+    UnknownSegment(u16),
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::InvalidArgs(line) => {
-                write!(f, "wrong number or type or arguments on line {}", line)
+                write!(f, "wrong arg number or type on line {}", line)
             }
             Self::Io(e) => write!(f, "cannot read: {}", e),
             Self::UnknownCommand(line) => write!(f, "unknown command on line {}", line),
+            Self::UnknownSegment(line) => write!(f, "unknown segment on line {}", line),
         }
     }
 }
@@ -27,6 +60,7 @@ impl std::error::Error for Error {
             Self::InvalidArgs(_) => None,
             Self::Io(e) => Some(e),
             Self::UnknownCommand(_) => None,
+            Self::UnknownSegment(_) => None,
         }
     }
 }
--- hack_virtual_machine/src/reader.rs
+++ hack_virtual_machine/src/reader.rs
@@ -1,8 +1,20 @@
 //! Reader for .vm files.
 
+use crate::{Command, Error, Segment};
+
+/// Reader that converts .vm files into [Command]s.
+///
+/// For each line, the reader determines how many args there are and populates the appropriate fields. It is up to the consumer to check arg numbers before parsing the fields. Using fields above the number read will get you fields from a previous line.
+///
+/// The safer implementation would be to use vector and options, but requires checking all elements and dealing with lots of match statements. With the arg number approach, one (non-enforced) check (how many args are there) provides all the guarantees.
+///
 pub struct Reader<R> {
     inner: R,
-    buffer: Option<Vec<String>>,
+    buffer: String,
+    args: u8,
+    cmd: String,
+    arg2: String,
+    arg3: u16,
     line: u16,
 }
 
@@ -10,12 +22,18 @@ impl<R: std::io::BufRead> Reader<R> {
     pub fn new(inner: R) -> Self {
         Self {
             inner,
-            buffer: None,
+            buffer: "".to_string(),
+            args: 0,
+            cmd: "".to_string(),
+            arg2: "".to_string(),
+            arg3: 0,
             line: 0,
         }
     }
 
-    /// Read the next line into the buffer and [cleans][clean_line] it. Returns true if line was read and false if EOF was reached.
+    /// Read the next line into the buffer and [cleans][clean_line] it.
+    ///
+    /// Returns the total number of lines read so far. Returns 0 if EOF has been reached.
     ///
     /// # Examples
     /// ```
@@ -27,337 +45,164 @@ impl<R: std::io::BufRead> Reader<R> {
     ///     reader.read_line()?,
     ///     reader.read_line()?,
     /// );
-    /// assert_eq!((a, b, c, d), (true, true, false, false));
+    /// assert_eq!((a, b, c, d), (1, 2, 0, 0));
     /// # Ok::<(), hack_virtual_machine::Error>(())
     /// ```
-    pub fn read_line(&mut self) -> Result<bool, crate::Error> {
-        let mut temp = "".to_string();
-        let read = self.inner.read_line(&mut temp)?;
+    pub fn read_line(&mut self) -> Result<u16, Error> {
+        self.buffer.clear();
+        let read = self.inner.read_line(&mut self.buffer)?;
         if read == 0 {
-            self.buffer = None;
-            Ok(false)
+            Ok(0)
         } else {
+            clean_line(&mut self.buffer);
+            self.args = 0;
             self.line += 1;
-            clean_line(&mut temp);
-            let v: Vec<String> = temp
-                .split_ascii_whitespace()
-                .map(|x| x.to_string())
-                .collect();
-            self.buffer = Some(v);
-            Ok(true)
-        }
-    }
+            let mut a = self.buffer.split_ascii_whitespace();
 
-    /// What command is currently in the buffer.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, CommandType, Arithmetic};
-    /// let input = b"\n//Nothing line\nadd \nweird";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.check_command(), Some(CommandType::EmptyLine));
-    /// reader.read_line()?;
-    /// assert_eq!(reader.check_command(), Some(CommandType::EmptyLine));
-    /// reader.read_line()?;
-    /// assert_eq!(reader.check_command(), Some(CommandType::Arithmetic(Arithmetic::Add)));
-    /// reader.read_line()?;
-    /// assert_eq!(reader.check_command(), Some(CommandType::Invalid));
-    /// reader.read_line()?;
-    /// assert_eq!(reader.check_command(), None);
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    /// ```
-    pub fn check_command(&self) -> Option<CommandType> {
-        match &self.buffer {
-            None => None,
-            Some(v) => {
-                if let Some(s) = v.get(0) {
-                    let com = match s.as_str() {
-                        "add" => CommandType::Arithmetic(Arithmetic::Add),
-                        "sub" => CommandType::Arithmetic(Arithmetic::Subtract),
-                        "neg" => CommandType::Arithmetic(Arithmetic::Negative),
-                        "eq" => CommandType::Arithmetic(Arithmetic::Equal),
-                        "gt" => CommandType::Arithmetic(Arithmetic::GreaterThan),
-                        "lt" => CommandType::Arithmetic(Arithmetic::LessThan),
-                        "and" => CommandType::Arithmetic(Arithmetic::BitAnd),
-                        "or" => CommandType::Arithmetic(Arithmetic::BitOr),
-                        "not" => CommandType::Arithmetic(Arithmetic::BitNot),
-                        "push" => CommandType::Push,
-                        "pop" => CommandType::Pop,
-                        "label" => CommandType::Label,
-                        "goto" => CommandType::Goto,
-                        "if-goto" => CommandType::If,
-                        "function" => CommandType::Function,
-                        "call" => CommandType::Call,
-                        "return" => CommandType::Return,
-                        _ => CommandType::Invalid,
-                    };
-                    Some(com)
-                } else {
-                    Some(CommandType::EmptyLine)
+            match a.next() {
+                None => return Ok(self.line),
+                Some(s) => {
+                    self.cmd.clear();
+                    self.cmd.push_str(s);
+                    self.args += 1;
                 }
+            };
+
+            match a.next() {
+                None => return Ok(self.line),
+                Some(s) => {
+                    self.arg2.clear();
+                    self.arg2.push_str(s);
+                    self.args += 1;
+                }
+            };
+
+            match a.next() {
+                None => return Ok(self.line),
+                Some(s) => {
+                    self.arg3 = s.parse().or(Err(Error::InvalidArgs(self.line)))?;
+                    self.args += 1;
+                }
+            };
+
+            match a.next() {
+                None => Ok(self.line),
+                Some(_) => Err(Error::InvalidArgs(self.line)),
             }
         }
     }
 
-    /// Returns error if buffer does not have the expected number of fields (command + args).
+    /// Read the next [cleaned][clean_line] command line into the buffer.
     ///
-    /// If nothing is in the buffer, returns `Ok`.
+    /// This will skip empty lines and comment only lines.
+    ///
+    /// Returns the line number of the command or 0 if EOF has been reached.
     ///
     /// # Examples
     /// ```
-    /// use hack_virtual_machine::reader::{Reader};
-    /// let input = b"Three args here";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert!(reader.error_buffer_len(3).is_ok());
-    /// assert!(reader.error_buffer_len(2).is_err());
-    /// reader.read_line()?;
-    /// assert!(reader.error_buffer_len(3).is_ok());
+    /// let input = b"// Create\n\nlife \njoyfully\n";
+    /// let mut reader = hack_virtual_machine::reader::Reader::new(&input[..]);
+    /// let (a, b, c) = (
+    ///     reader.read_command()?,
+    ///     reader.read_command()?,
+    ///     reader.read_command()?,
+    /// );
+    /// assert_eq!((a, b, c), (3, 4, 0));
     /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn error_buffer_len(&self, len: usize) -> Result<(), crate::Error> {
-        if let Some(buf) = &self.buffer {
-            if buf.len() == len {
-                Ok(())
-            } else {
-                Err(crate::Error::InvalidArgs(self.line))
+    /// ```
+    pub fn read_command(&mut self) -> Result<u16, Error> {
+        let mut line = self.read_line()?;
+
+        while line != 0 && self.args == 0 {
+            line = self.read_line()?;
+        }
+
+        Ok(line)
+    }
+
+    /// Parse the command currently in the buffer.
+    ///
+    /// This assumes there is a command line in the buffer.
+    ///
+    /// # Examples
+    /// ```
+    /// let input = b"//My program\nadd\n\nnot";
+    /// let mut reader = hack_virtual_machine::reader::Reader::new(&input[..]);
+    /// reader.read_command()?;
+    /// let add = reader.parse_command()?;
+    /// assert_eq!(add, hack_virtual_machine::Command::Add);
+    /// reader.read_command()?;
+    /// let not = reader.parse_command()?;
+    /// assert_eq!(not, hack_virtual_machine::Command::BitNot);
+    /// # Ok::<(), hack_virtual_machine::Error>(())
+    /// ```
+    pub fn parse_command(&mut self) -> Result<Command, Error> {
+        match self.cmd.as_str() {
+            "add" => {
+                self.assert_args(1)?;
+                Ok(Command::Add)
             }
+            "sub" => {
+                self.assert_args(1)?;
+                Ok(Command::Subtract)
+            }
+            "neg" => {
+                self.assert_args(1)?;
+                Ok(Command::Negative)
+            }
+            "eq" => {
+                self.assert_args(1)?;
+                Ok(Command::Equal)
+            }
+            "gt" => {
+                self.assert_args(1)?;
+                Ok(Command::GreaterThan)
+            }
+            "lt" => {
+                self.assert_args(1)?;
+                Ok(Command::LessThan)
+            }
+            "and" => {
+                self.assert_args(1)?;
+                Ok(Command::Add)
+            }
+            "or" => {
+                self.assert_args(1)?;
+                Ok(Command::BitOr)
+            }
+            "not" => {
+                self.assert_args(1)?;
+                Ok(Command::BitNot)
+            }
+            "push" => {
+                self.assert_args(3)?;
+                Ok(Command::Push(
+                    self.arg2
+                        .parse()
+                        .or(Err(Error::UnknownSegment(self.line)))?,
+                    self.arg3,
+                ))
+            }
+            "pop" => {
+                self.assert_args(3)?;
+                Ok(Command::Pop(
+                    self.arg2
+                        .parse()
+                        .or(Err(Error::UnknownSegment(self.line)))?,
+                    self.arg3,
+                ))
+            }
+            _ => Err(Error::UnknownCommand(self.line)),
+        }
+    }
+
+    fn assert_args(&self, num: u8) -> Result<(), Error> {
+        if num != self.args {
+            Err(Error::InvalidArgs(self.line))
         } else {
             Ok(())
         }
     }
-
-    /// Parse the segment and index arg of a push or pop command.
-    ///
-    /// This assumes the line is at an push or pop command.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Segment};
-    /// let input = b"push_or_pop static 3";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_args_push_pop()?, Some((Segment::Static, 3)));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_args_push_pop(&self) -> Result<Option<(Segment, u16)>, crate::Error> {
-        if let Some(buf) = &self.buffer {
-            let s = buf
-                .get(1)
-                .ok_or(crate::Error::InvalidArgs(self.line))?
-                .parse()
-                .or(Err(crate::Error::InvalidArgs(self.line)))?;
-            let i = buf
-                .get(2)
-                .ok_or(crate::Error::InvalidArgs(self.line))?
-                .parse()
-                .or(Err(crate::Error::InvalidArgs(self.line)))?;
-            Ok(Some((s, i)))
-        } else {
-            Ok(None)
-        }
-    }
-
-    /// Parse the symbol arg.
-    ///
-    /// Assumes the line has a symbol as the first arg.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::Reader;
-    /// let input = b"command symbol_is_best";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_args_symbol()?, Some(&"symbol_is_best".to_string()));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_args_symbol(&self) -> Result<Option<&String>, crate::Error> {
-        if let Some(buf) = &self.buffer {
-            Ok(Some(
-                buf.get(1).ok_or(crate::Error::InvalidArgs(self.line))?,
-            ))
-        } else {
-            Ok(None)
-        }
-    }
-
-    /// Parse an arithmetic command.
-    ///
-    /// This assumes the line is at the arithmetic command specified to the function.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Command, Arithmetic};
-    /// let input = b"just_one_command";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_command_arithmetic(Arithmetic::Add)?, Some(Command::Arithmetic(Arithmetic::Add)));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_command_arithmetic(
-        &self,
-        kind: Arithmetic,
-    ) -> Result<Option<Command>, crate::Error> {
-        self.error_buffer_len(1)?;
-        match &self.buffer {
-            Some(_) => Ok(Some(Command::Arithmetic(kind))),
-            None => Ok(None),
-        }
-    }
-
-    /// Parse a goto command.
-    ///
-    /// Assumes line is at a goto command.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Command};
-    /// let input = b"com_not_checked give_me_a_symbol";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_command_goto()?, Some(Command::Goto("give_me_a_symbol".to_string())));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_command_goto(&self) -> Result<Option<Command>, crate::Error> {
-        self.error_buffer_len(2)?;
-        match self.parse_args_symbol()? {
-            Some(s) => Ok(Some(Command::Goto(s.to_owned()))),
-            None => Ok(None),
-        }
-    }
-
-    /// Parse a if command.
-    ///
-    /// Assumes line is at a if command.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Command};
-    /// let input = b"com_not_checked give_me_a_symbol";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_command_if()?, Some(Command::If("give_me_a_symbol".to_string())));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_command_if(&self) -> Result<Option<Command>, crate::Error> {
-        self.error_buffer_len(2)?;
-        match self.parse_args_symbol()? {
-            Some(s) => Ok(Some(Command::If(s.to_owned()))),
-            None => Ok(None),
-        }
-    }
-
-    /// Parse a label command.
-    ///
-    /// Assumes line is at a label command.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Command};
-    /// let input = b"label_com_not_checked give_me_a_symbol";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_command_label()?, Some(Command::Label("give_me_a_symbol".to_string())));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_command_label(&self) -> Result<Option<Command>, crate::Error> {
-        self.error_buffer_len(2)?;
-        match self.parse_args_symbol()? {
-            Some(s) => Ok(Some(Command::Label(s.to_owned()))),
-            None => Ok(None),
-        }
-    }
-
-    /// Parse the pop command.
-    ///
-    /// Assumes the line is on a pop command.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Command, Segment};
-    /// let input = b"does_not_matter temp 4";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_command_pop()?, Some(Command::Pop(Segment::Temp, 4)));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_command_pop(&self) -> Result<Option<Command>, crate::Error> {
-        self.error_buffer_len(3)?;
-        if let Some((s, i)) = self.parse_args_push_pop()? {
-            Ok(Some(Command::Pop(s, i)))
-        } else {
-            Ok(None)
-        }
-    }
-
-    /// Parse the push command.
-    ///
-    /// Assumes the line is on a push command.
-    ///
-    /// # Examples
-    /// ```
-    /// use hack_virtual_machine::reader::{Reader, Command, Segment};
-    /// let input = b"does_not_matter local 2";
-    /// let mut reader = Reader::new(&input[..]);
-    /// reader.read_line()?;
-    /// assert_eq!(reader.parse_command_push()?, Some(Command::Push(Segment::Local, 2)));
-    /// # Ok::<(), hack_virtual_machine::Error>(())
-    pub fn parse_command_push(&self) -> Result<Option<Command>, crate::Error> {
-        self.error_buffer_len(3)?;
-        if let Some((s, i)) = self.parse_args_push_pop()? {
-            Ok(Some(Command::Push(s, i)))
-        } else {
-            Ok(None)
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum Command {
-    Arithmetic(Arithmetic),
-    Goto(String),
-    If(String),
-    Label(String),
-    Pop(Segment, u16),
-    Push(Segment, u16),
-}
-
-/// The commands that the VM supports.
-///
-/// The extra ones needed for my implementation are
-/// * `EmptyLine`: a line that is empty (or comment only) and does nothing
-/// * `Invalid`: a line that has an unknown command
-#[derive(Debug, PartialEq, Eq)]
-pub enum CommandType {
-    Arithmetic(Arithmetic),
-    Push,
-    Pop,
-    Label,
-    Goto,
-    If,
-    Function,
-    Return,
-    Call,
-    EmptyLine,
-    Invalid,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum Arithmetic {
-    Add,
-    Subtract,
-    Negative,
-    Equal,
-    GreaterThan,
-    LessThan,
-    BitAnd,
-    BitOr,
-    BitNot,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum Segment {
-    Argument,
-    Local,
-    Static,
-    Constant,
-    This,
-    That,
-    Pointer,
-    Temp,
 }
 
 impl std::str::FromStr for Segment {
@@ -390,6 +235,9 @@ impl std::str::FromStr for Segment {
 /// let mut s = " Code //Comment".to_string();
 /// clean_line(&mut s);
 /// assert_eq!(s, "Code".to_string());
+/// s = " // Double // Comment".to_string();
+/// clean_line(&mut s);
+/// assert_eq!(s, "".to_string());
 /// ```
 pub fn clean_line(line: &mut String) {
     let no_comments = match line.split_once("//") {
