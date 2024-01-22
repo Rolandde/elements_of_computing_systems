88ad0bc3f7c0d20bde0b33dafbf1393a4aec8a76
Savo Lazic <lazicsavo@gmail.com>
2024-01-21 22:02:04 -0500

    `gt` assembly code written for the VM

--- hack_virtual_machine/src/lib.rs
+++ hack_virtual_machine/src/lib.rs
@@ -105,18 +105,50 @@ pub fn eq() -> [Assembly; 24] {
         CCommand::new_dest(CDest::M, CComp::D).into(), // M[0] = 1, stack pointer is at subtraction result
         CCommand::new_dest(CDest::A, CComp::M).into(), // A = M[0] = 1
         CCommand::new_dest(CDest::D, CComp::M).into(), // D = M[1] = X - Y
-        ACommand::Symbol("EQUAL".to_string()).into(),  // @EQUAL
+        ACommand::Symbol("EQ_TRUE".to_string()).into(), // Label to jump to if equal
         CCommand::new_jump(CComp::D, CJump::Equal).into(), // Jump to EQUAL if D is 0, otherwise not equal
         ReservedSymbols::SP.into(),                        // A = 0
         CCommand::new_dest(CDest::A, CComp::M).into(), // A = M[0] = 1 (address where true/false needs to be written)
         CCommand::new_dest(CDest::M, CComp::Zero).into(), // M[1] = 0
-        ACommand::Symbol("EQUAL_DONE".to_string()).into(), // Jump to finishing equal assembly
+        ACommand::Symbol("EQ_DONE".to_string()).into(), // Jump to finishing equal assembly
         CCommand::new_jump(CComp::Zero, CJump::Jump).into(), // Always jump to finishing assembly
-        Assembly::Label("EQUAL".to_string()),          // Start of code for equality
+        Assembly::Label("EQ_TRUE".to_string()),        // Start of code for equality
         ReservedSymbols::SP.into(),                    // A = 0
         CCommand::new_dest(CDest::A, CComp::M).into(), // A = M[0] = 1, (address where true/false needs to be written)
         CCommand::new_dest(CDest::M, CComp::MinumOne).into(), // M[1] = -1 (true, X and Y are equal)
-        Assembly::Label("EQUAL_DONE".to_string()),     // Code for finishing up equality
+        Assembly::Label("EQ_DONE".to_string()),        // Code for finishing up equality
+        ReservedSymbols::SP.into(),                    // A = 0
+        CCommand::new_dest(CDest::M, CComp::MPlusOne).into(), // M[0] = M[0] + 1 = 1 + 1 = 2
+    ]
+}
+
+/// Virtual machine stack greater than
+pub fn gt() -> [Assembly; 24] {
+    // For the comments, assume stack pointer at memory 0 points to 3
+    // The two numbers to compare at memory 1(X) and 2(Y)
+    [
+        ReservedSymbols::SP.into(), // A = 0 (stack pointer)
+        CCommand::new_dest(CDest::A, CComp::MMinusOne).into(), // A = M[0] - 1 = 3 - 1 = 2 (top of stack)
+        CCommand::new_dest(CDest::D, CComp::M).into(),         // D = M[2] = Y
+        CCommand::new_dest(CDest::A, CComp::AMinusOne).into(), // A = 1
+        CCommand::new_dest(CDest::M, CComp::MMinusD).into(),   // M[1] = X - Y
+        CCommand::new_dest(CDest::D, CComp::A).into(),         // D = 1
+        ReservedSymbols::SP.into(),                            // A = 0
+        CCommand::new_dest(CDest::M, CComp::D).into(), // M[0] = 1, stack pointer is at subtraction result
+        CCommand::new_dest(CDest::A, CComp::M).into(), // A = M[0] = 1
+        CCommand::new_dest(CDest::D, CComp::M).into(), // D = M[1] = X - Y
+        ACommand::Symbol("GT_TRUE".to_string()).into(), // Label to jump to if greater than
+        CCommand::new_jump(CComp::D, CJump::Greater).into(), // Jump if D is greater than 0, otherwise not greater than
+        ReservedSymbols::SP.into(),                          // A = 0
+        CCommand::new_dest(CDest::A, CComp::M).into(), // A = M[0] = 1 (address where true/false needs to be written)
+        CCommand::new_dest(CDest::M, CComp::Zero).into(), // M[1] = 0
+        ACommand::Symbol("GT_DONE".to_string()).into(), // Jump to setting stack pointer
+        CCommand::new_jump(CComp::Zero, CJump::Jump).into(), // Always jump to finishing assembly
+        Assembly::Label("GT_TRUE".to_string()),        // Start of code for greater than being true
+        ReservedSymbols::SP.into(),                    // A = 0
+        CCommand::new_dest(CDest::A, CComp::M).into(), // A = M[0] = 1, (address where true/false needs to be written)
+        CCommand::new_dest(CDest::M, CComp::MinumOne).into(), // M[1] = -1 (true, X and Y are equal)
+        Assembly::Label("GT_DONE".to_string()),        // Code for setting stack pointer
         ReservedSymbols::SP.into(),                    // A = 0
         CCommand::new_dest(CDest::M, CComp::MPlusOne).into(), // M[0] = M[0] + 1 = 1 + 1 = 2
     ]
@@ -281,5 +313,20 @@ mod vm_tests {
     #[test]
     fn test_eq_false() {
         stack_test(&eq(), 3, -100, Some(100), vec![MemVal(1, 0), MemVal(0, 2)])
+    }
+
+    #[test]
+    fn test_gt() {
+        stack_test(&gt(), 3, -100, Some(-99), vec![MemVal(1, -1), MemVal(0, 2)])
+    }
+
+    #[test]
+    fn test_gt_false_eq() {
+        stack_test(&gt(), 3, 1, Some(1), vec![MemVal(1, 0), MemVal(0, 2)])
+    }
+
+    #[test]
+    fn test_gt_false_less() {
+        stack_test(&gt(), 3, 10, Some(1), vec![MemVal(1, 0), MemVal(0, 2)])
     }
 }
